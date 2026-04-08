d9a5143756d351317da08d486c89aad78bb74f82
pablangas28 <pablitoerira28@gmail.com>
2026-04-08 17:59:08 -0500

    Se actualizo el formularioInscripcion.php para evitar conflictos en mysql

--- app/Models/FormularioInscripcion.php
+++ app/Models/FormularioInscripcion.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 
 class FormularioInscripcion extends Model
 {
+    protected $table = 'formularios_inscripcion';
+    
     protected $fillable = [
         'curso_id',
         'creado_por',
